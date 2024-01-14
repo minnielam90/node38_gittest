968f250f26bc3299c67d82c33abef0096860788f
minnielam90 <minnie.lam90@gmail.com>
2024-01-14 10:18:14 +0700

    B5

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("learning github");
+
+// B1: git status -> kiểm tra những file thay đổi nội dung hoặc file mới tạo
+
+// B2: git add . => add toàn bộ những file thay đổi và những file mới tạo
+
+// git add <path file>
+
+// B3: git commit -m "<nội dung commit>"
+
+// B4: git push origin <branch name>
+
+// tạo branch mới
+// git checkout -b <tên branch>
